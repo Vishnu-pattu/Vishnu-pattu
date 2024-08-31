7762391001db4c8f747e30d416c6570508622fcd
Vishnu-pattu <pvishnupriya211005@gmail.com>
2024-08-31 23:08:09 +0530

    Update parvathy.js

--- parvathy.js
+++ parvathy.js
@@ -1,14 +1,1 @@
-var a=10
-var b=40
-const c=4
-console.log(a+b)
-console.log(a)
-console.log(b)
-console.log(typeof(a))
- var price=100
- var product = apple15
- var ta=70
- console.log(product)
- tp=price+ta
- console.log(tp)
- 
+
