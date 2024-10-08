9aa9e20f5fa4a0fdd08b398b4fecf6590b7a4bee
Vishnu-pattu <pvishnupriya211005@gmail.com>
2024-10-08 16:15:03 +0530

    Update e1.js

--- EP1/e1.js
+++ EP1/e1.js
@@ -5,10 +5,8 @@ document.getElementById('contact-form').addEventListener('submit', function(e) {
     const email = document.getElementById('email').value;
     const message = document.getElementById('message').value;
 
-    // Here you can add functionality to send the form data to a server
     alert(`Thank you for your message, ${name}!`);
     
     // Clear the form
     this.reset();
 });
-
